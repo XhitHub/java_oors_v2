37821a1d664cd4daaf3b80c371cd439961b223d9
XhitHub <com.trojan.exe@gmail.com>
2021-06-25 18:19:17 +0800

    init

---
+++ src/main/java/com/example/oors/OorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.oors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OorsApplication.class, args);
+	}
+
+}
